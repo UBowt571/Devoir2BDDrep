1d77f634979faf729f038f29b74c24f3ef203d3e
Defrances Maxime <maxime.defrances@hotmail.fr>
2019-11-03 19:18:08 -0500

    getMonstersLinks OK

--- src/main/scala/MainObj.scala
+++ src/main/scala/MainObj.scala
@@ -1,13 +1,12 @@
 import java.io.File
 
 import org.apache.spark.{SparkConf, SparkContext}
-
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
-import scala.util.matching
 import scala.util.matching.Regex
 
-case class monster(var name: String, var spells: ListBuffer[Int])
+case class monster(var name: String, var spells: ListBuffer[spell])
+case class spell(var id:Int,var name:String)
 
 object MainObj  extends  App {
 
@@ -16,8 +15,9 @@ object MainObj  extends  App {
     .setMaster("local[*]")
   val sc = new SparkContext(conf)
   sc.setLogLevel("ERROR")
-  var shortened_file = getFileFromRelativePath(1,"Devoir2BDDrep_data\\bestiary\\angel.html");
-  getMonsters(shortened_file);
+  var file = getFileFromRelativePath(1,"Devoir2BDDrep_data\\indices\\bestiary.html");
+  var monstersLinks = getMonsterFromIndex(file)
+  var monstersList = new ListBuffer[monster]
 
   var test = 50
 
@@ -31,25 +31,30 @@ object MainObj  extends  App {
     path +=("\\"+filename)
     var html = Source.fromURL("file:///"+path)
     var s = html.mkString
-    s = s.substring(s.indexOf("<div class = \"body\">"))
-    s = s.substring(0,s.indexOf("</div>")+6)
     s
   }
 
-  def getMonsters(page : String):ListBuffer[String]={
-    var monsters = new ListBuffer[String]
-    var regex = new Regex("<h1 id=\"[A-Za-z-]*\">[A-Za-z-, ]*</h1>\n\t*<p class=\"flavor-text\">|<h1 id=\"[A-Za-z-]*\">[A-Za-z-, ]*</h1>\n\t*\n\t*<p class=\"flavor-text\">").findAllMatchIn(page)
+  def getMonsterFromIndex(source:String)={
+    var openingTag = "<ul id=\"index-monsters-[A-z]\" title=\"[A-z] Monsters\">\n*[ \t]*<li class"
+    var closingTag = "</a></li>\\n*</ul>"
+    var regex = new Regex(openingTag+"|"+closingTag).findAllMatchIn(source)
+    var currentSrcTexte = source.substring(regex.next().start,regex.next().end)     // Select one monster from the whole list
+    var regexMonstre = new Regex("href *= *\"[a-zA-Z0-9./#-]*\"").findAllMatchIn(currentSrcTexte)   // find links in the monster's line
+    var listMonsters = new ListBuffer[String];
 
-    while (regex.hasNext){
-      var currentMatch = regex.next()
-      if(page.substring(currentMatch.start+1).indexOf("<h1 ") == -1){
-        monsters+= page.substring(currentMatch.start,page.indexOf("</div>"))
-      }else{
-        var nextH1Title = page.substring(currentMatch.start + 1 )
-        var endofsubstr = nextH1Title.indexOf("<h1")
-        monsters+= page.substring(currentMatch.start,currentMatch.start + endofsubstr - 1)
-      }
+    while (regexMonstre.hasNext) {                  // For every link of this line
+      val currentMatch = regexMonstre.next()        // Current link
+      val currentLinkStart = currentMatch.start     // Current link start index
+      val currentLinkEnd = currentMatch.end         // Current link end index
+      var currentLink = currentSrcTexte.substring(currentLinkStart,currentLinkEnd)
+      val endOfHrefIndex = currentLink.indexOf("/",1)
+      currentLink = currentLink.substring(endOfHrefIndex).replace("/","\\");
+      if(listMonsters.nonEmpty){
+        if ( ! listMonsters.last.equals(currentLink)){      // If link is identical to the last one, don't add (should be if link is already in list)
+          listMonsters+=currentLink
+        }
+      }else{listMonsters+=currentLink}
     }
-    monsters
+    listMonsters
   }
 }
